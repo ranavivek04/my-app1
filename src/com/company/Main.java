8709599a95f71f7a9c21948f7700571b7249965a
Vivek Rana <vivek.rana@nuance.com>
2020-02-17 12:12:29 +0530

    No print statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
-	// write your code here
-        System.out.println("Hey There");    
-	    }
+    }
 }
