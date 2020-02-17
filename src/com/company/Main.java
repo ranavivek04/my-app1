7ded0b1e54942e3f86bf8f01cecb74fa0bc064f8
Vivek Rana <vivek.rana@nuance.com>
2020-02-17 11:26:25 +0530

    changes done Hello world 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,5 @@ public class Main {
         System.out.println("Hello World");
 	// write your code here
         System.out.println("Hey There");    
-	// write your code here
-        System.out.println("Good Morning!");     
-    }
+	    }
 }
