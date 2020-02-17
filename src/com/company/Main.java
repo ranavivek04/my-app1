6f61efc27663e2848ce3878c57334711769c19e2
ranavivek04 <43881115+ranavivek04@users.noreply.github.com>
2020-02-17 12:41:14 +0530

    added hello world 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 	    System.out.println("Hello World");
+	// write your code here2
+	    System.out.println("Hello World2");    
     }
 }
