c551e0b4038f3f79dd3ca4bc579352e857ecb571
ranavivek04 <43881115+ranavivek04@users.noreply.github.com>
2020-02-17 11:08:49 +0530

    removed 2 println statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
-	System.out.println("Hello World1");    
-	// changes done by other developer    
-
-        System.out.println("Hello World3");
-        // changes done by other developer3
-    }
+	}
 }
